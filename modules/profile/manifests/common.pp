a2c5546c66eb9da90d65490d3688ea58fcf6407d
Nick Jones <nick@dischord.org>
2016-05-20 10:15:16 +0100

    Install fish and nmap, make sure Puppet isn't running

--- modules/profile/manifests/common.pp
+++ modules/profile/manifests/common.pp
@@ -2,7 +2,13 @@
 #
 class profile::common {
 
+  include ::apt
+
   create_resources(user, hiera('users'))
   create_resources(package, hiera('packages'))
+
+  service { 'puppet':
+    ensure => stopped,
+  }
 
 }
